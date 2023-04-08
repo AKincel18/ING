902e553539565ae006cb9134ab016bbae80691fe
AKincel18 <akincel97@gmail.com>
2023-04-08 18:51:21 +0200

    Refactor AtmService.

--- src/main/java/ing/contest/atmservice/service/AtmService.java
+++ src/main/java/ing/contest/atmservice/service/AtmService.java
@@ -10,87 +10,36 @@ import java.util.*;
 @Service
 public class AtmService {
     public List<Atm> calculateOrder(List<ServiceTask> serviceTasks) {
-        removeDuplicatedServices(serviceTasks);
+        filterByHighestPriority(serviceTasks);
         Map<Integer, Map<RequestType, List<ServiceTask>>> routesByRegion = calculateRoutesByRegion(serviceTasks);
 
-        return new ArrayList<>(routesByRegion.values()
+        return routesByRegion.entrySet()
                 .stream()
-                .flatMap(map -> map.entrySet().stream())
-                .flatMap(map -> map.getValue().stream())
+                .flatMap(entry -> entry.getValue().entrySet().stream())
+                .flatMap(entry -> entry.getValue().stream())
                 .map(Atm::new)
-                .toList());
+                .toList();
     }
 
-    private void removeDuplicatedServices(List<ServiceTask> serviceTasks) {
-        Set<String> atmIds = new HashSet<>();
-        Map<String, ServiceTask> servicesByIds = new HashMap<>();
-        List<ServiceTask> servicesToRemove = new ArrayList<>();
-        serviceTasks.forEach(serviceTask -> {
-                    if (atmIds.add(serviceTask.toString())) {
-                        servicesByIds.put(serviceTask.toString(), serviceTask);
-                    }
-                    else {
-                        ServiceTask task = servicesByIds.get(serviceTask.toString());
-                        if (serviceTask.getRequestType().isHighPriority(task.getRequestType())) {
-                            servicesToRemove.add(task);
-                            servicesByIds.put(serviceTask.toString(), serviceTask);
-                        } else {
-                            servicesToRemove.add(serviceTask);
-                        }
-                    }
-                }
-        );
-        serviceTasks.removeAll(servicesToRemove);
+    private void filterByHighestPriority(List<ServiceTask> serviceTasks) {
+        Map<String, ServiceTask> uniqueServices = new HashMap<>();
+        serviceTasks.stream()
+                .filter(serviceTask -> isTaskUnique(serviceTask, uniqueServices.get(serviceTask.getRegionAtmId())))
+                .forEach(serviceTask -> uniqueServices.put(serviceTask.getRegionAtmId(), serviceTask));
+        serviceTasks.retainAll(uniqueServices.values());
     }
 
-//    private List<ServiceTask> filterByHighestPriority(List<ServiceTask> serviceTasks) {
-//        return serviceTasks.stream()
-//                .collect(Collectors.groupingBy(ServiceTask::getRegionAtmId, Collectors.maxBy(Comparator.comparing(ServiceTask::getRequestType))))
-//                .values().stream()
-//                .flatMap(Optional::stream)
-//                .collect(Collectors.toList());
-//    }
-//
-//    private List<ServiceTask> filterByHighestPriority2(List<ServiceTask> serviceTasks) {
-//        Map<String, ServiceTask> taskMap = new HashMap<>();
-//        for (ServiceTask task : serviceTasks) {
-//            String key = task.getRegion() + "-" + task.getAtmId();
-//            ServiceTask existingTask = taskMap.get(key);
-//            if (existingTask == null || task.getRequestType().isHighPriority(existingTask.getRequestType())) {
-//                taskMap.put(key, task);
-//            }
-//        }
-//        return new ArrayList<>(taskMap.values());
-//    }
+    private boolean isTaskUnique(ServiceTask serviceTask, ServiceTask existingTask) {
+        return existingTask == null || serviceTask.getRequestType().isHighPriority(existingTask.getRequestType());
+    }
 
     private Map<Integer, Map<RequestType, List<ServiceTask>>> calculateRoutesByRegion(List<ServiceTask> serviceTasks) {
         Map<Integer, Map<RequestType, List<ServiceTask>>> routesByRegion = new TreeMap<>(Comparator.naturalOrder());
-        serviceTasks.forEach(serviceTask -> {
-            if (routesByRegion.containsKey(serviceTask.getRegion())) {
-                Map<RequestType, List<ServiceTask>> tasksInRegion = routesByRegion.get(serviceTask.getRegion());
-                if (tasksInRegion.containsKey(serviceTask.getRequestType())) {
-                    tasksInRegion.get(serviceTask.getRequestType()).add(serviceTask);
-                } else {
-                    tasksInRegion.put(serviceTask.getRequestType(), new ArrayList<>(Collections.singletonList(serviceTask)));
-                }
-            } else {
-                Map<RequestType, List<ServiceTask>> routesByTypeInRegion = new TreeMap<>(Comparator.comparing(RequestType::ordinal));
-                routesByTypeInRegion.put(serviceTask.getRequestType(), new ArrayList<>(Collections.singletonList(serviceTask)));
-                routesByRegion.put(serviceTask.getRegion(), routesByTypeInRegion);
-            }
-        });
+        serviceTasks.forEach(serviceTask -> routesByRegion
+                .computeIfAbsent(serviceTask.getRegion(), key -> new TreeMap<>(Comparator.comparing(RequestType::ordinal)))
+                .computeIfAbsent(serviceTask.getRequestType(), key -> new ArrayList<>())
+                .add(serviceTask)
+        );
         return routesByRegion;
     }
-
-//    private Map<Integer, Map<RequestType, List<ServiceTask>>> calculateRoutesByRegion(List<ServiceTask> serviceTasks) {
-//        Map<Integer, Map<RequestType, List<ServiceTask>>> routesByRegion = new TreeMap<>(Comparator.naturalOrder());
-//        serviceTasks.forEach(serviceTask ->
-//                routesByRegion
-//                        .computeIfAbsent(serviceTask.getRegion(), key -> new TreeMap<>(Comparator.comparing(RequestType::ordinal)))
-//                        .computeIfAbsent(serviceTask.getRequestType(), key -> new ArrayList<>())
-//                        .add(serviceTask)
-//        );
-//        return routesByRegion;
-//    }
-
 }
