62bfd8096fcd70984ca73099d9686c5cf93e9769
AKincel18 <akincel97@gmail.com>
2023-04-10 14:41:02 +0200

    Add sorting as a first step of algorithm.

--- src/main/java/ing/contest/onlinegame/model/Clan.java
+++ src/main/java/ing/contest/onlinegame/model/Clan.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Clan {
     @Min(1)
     @Max(1000)
--- src/main/java/ing/contest/onlinegame/service/OnlineGameService.java
+++ src/main/java/ing/contest/onlinegame/service/OnlineGameService.java
@@ -5,11 +5,14 @@ import ing.contest.onlinegame.model.Players;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class OnlineGameService {
     public List<List<Clan>> calculate(Players players) {
+        players.getClans().sort(Comparator.comparing(Clan::getPoints).reversed().thenComparing(Clan::getNumberOfPlayers));
+        players.getClans().forEach(clan -> System.out.println(clan.toString()));
         return new ArrayList<>();
     }
 }
