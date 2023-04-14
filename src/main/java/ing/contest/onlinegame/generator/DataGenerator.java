449946a98e7aaf1581a0cb62746338f4604e3624
AKincel18 <akincel97@gmail.com>
2023-04-14 20:14:38 +0200

    Refactor generator.

--- src/main/java/ing/contest/onlinegame/generator/DataGenerator.java
+++ src/main/java/ing/contest/onlinegame/generator/DataGenerator.java
@@ -6,18 +6,19 @@ import ing.contest.onlinegame.model.Players;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class DataGenerator {
     public static void generateData() {
-        int groupCount = 100;
+        int groupCount = 1000;
         int clanSize = 20000;
         List<Clan> clans = new ArrayList<>();
         for (int i = 0; i < clanSize; i++) {
-            int numberOfPlayers = new Random().nextInt(groupCount - 60) + 40;
-            int points = new Random().nextInt(40) + 40;
+            int numberOfPlayers = new Random().nextInt(groupCount) + 1;
+            int points = new Random().nextInt(groupCount) + 1;
             Clan clan = new Clan(numberOfPlayers, points);
             clans.add(clan);
         }
@@ -25,17 +26,9 @@ public class DataGenerator {
 
         ObjectMapper mapper = new ObjectMapper();
         try {
-            //Convert object to JSON string and save into file directly
-            mapper.writeValue(new File("C:/Users/akincel/Repos/ING/src/main/java/ing/contest/onlinegame/generator/players.json"), players);
-
-            //Convert object to JSON string
-            String jsonInString = mapper.writeValueAsString(players);
-//            System.out.println(jsonInString);
-
-            //Convert object to JSON string and pretty print
-            jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(players);
-//            System.out.println(jsonInString);
-
+            String pathName = Path.of("").toAbsolutePath() + "\\src\\main\\java\\ing\\contest\\onlinegame\\generator\\players.json";
+            File file = new File(pathName);
+            mapper.writeValue((file), players);
 
         } catch (IOException e) {
             e.printStackTrace();
