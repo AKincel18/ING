b9dc044e04129026ef5a707ebc1858e1e8a13605
akincel <akincel97@gmail.com>
2023-04-20 16:43:00 +0200

    Add first working algorithm.

--- src/main/java/ing/contest/transactions/model/Account.java
+++ src/main/java/ing/contest/transactions/model/Account.java
@@ -10,7 +10,31 @@ import lombok.*;
 public class Account {
     //todo add fields validation
     private String account;
-    private String debitCount;
-    private String creditCount;
+    private int debitCount;
+    private int creditCount;
     private float balance;
+
+    public Account(String account) {
+        this.account = account;
+    }
+
+    public void incrementDebitCount() {
+        debitCount += 1;
+    }
+
+    public void incrementCreditCount() {
+        creditCount += 1;
+    }
+
+    public void increaseBalance(float amount) {
+        balance += amount;
+    }
+
+    public void decreaseBalance(float amount) {
+        balance -= amount;
+    }
+
+    public void roundBalance() {
+        balance = (float) (Math.round(balance * 10.0) / 10.0);
+    }
 }
--- src/main/java/ing/contest/transactions/service/TransactionService.java
+++ src/main/java/ing/contest/transactions/service/TransactionService.java
@@ -4,12 +4,39 @@ import ing.contest.transactions.model.Account;
 import ing.contest.transactions.model.Transaction;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TransactionService {
+    private final Map<String, Account> report = new HashMap<>();
+
     public List<Account> createReport(List<Transaction> transactions) {
-        return new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            Account debitAccount = getAccountFromReport(transaction.getDebitAccount());
+            Account creditAccount = getAccountFromReport(transaction.getCreditAccount());
+
+            debitAccount.incrementDebitCount();
+            creditAccount.incrementCreditCount();
+
+            debitAccount.decreaseBalance(transaction.getAmount());
+            creditAccount.increaseBalance(transaction.getAmount());
+
+            debitAccount.roundBalance();
+            creditAccount.roundBalance();
+        }
+
+        return report.values()
+                .stream()
+                .sorted(Comparator.comparing(Account::getAccount))
+                .toList();
+    }
+
+    private Account getAccountFromReport(String accountString) {
+        Account account = report.putIfAbsent(accountString, new Account(accountString));
+        return account != null ? account : report.get(accountString);
     }
 }
